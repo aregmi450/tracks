1e8ba555d3b74d610478297a20153b91cad84ec3
Arun Regmi <aregmi450@gmail.com>
2021-11-02 14:04:12 +0545

    added tracks

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'http://c53d-163-47-149-193.ngrok.io', 
+    baseURL: 'http://43e1-163-47-149-197.ngrok.io',
     //change the URL every 2 hours
 });
